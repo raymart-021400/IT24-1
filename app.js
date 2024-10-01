65983ae6c61e9886b4de077a391671187ef1d7c7
raymart-021400 <avancenaraymart62@gmail.com>
2024-10-01 09:57:09 +0800

    Added arithmetic and logical operators

--- app.js
+++ app.js
@@ -13,3 +13,32 @@ let middlename;
 
 //null
 let firstname = null;
+
+//B. Arithmetic and logical eperators
+//Arithmetic
+let x = 10;
+let y = 20;
+
+let sum = x + y;
+let diff = x - y;
+let prod = x * y;
+let qou = x / y;
+
+console.log("the sum is " + sum);
+console.log("the difference is " + diff);
+console.log("the product is " + prod);
+console.log("the qoutient is " + qou );
+
+//Logical Operators
+ let num1 = 5;
+ let num2 = 10;
+
+ let result1 = num1 += 20;
+ let result2 = num1 += 20;
+ let result3 = num1 += 20;
+ let result4 = num1 += 20;
+
+ console.log(result1);
+ console.log(result2);
+ console.log(result3);
+ console.log(result4);
